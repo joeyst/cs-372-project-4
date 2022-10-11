0b6100be557deef6b774f9faf17d0f4d755ba612
joeyst <joeystenbeck@gmail.com>
2022-10-11 11:20:02 -0700

    Add system time and close connection.

--- Project 4.1/timeclient.py
+++ Project 4.1/timeclient.py
@@ -1,4 +1,4 @@
-import socket
+import socket, datetime
 
 server_name, port = 'time.nist.gov', 37
 s = socket.socket()
@@ -15,6 +15,22 @@ text_data_send = byte_data_send.encode()
 
 s.sendall(text_data_send)
 
+# taken from assignment
+def system_seconds_since_1900():
+    """
+    The time server returns the number of seconds since 1900, but Unix
+    systems return the number of seconds since 1970. This function
+    computes the number of seconds since 1900 on the system.
+    """
+
+    # Number of seconds between 1900-01-01 and 1970-01-01
+    seconds_delta = 2208988800
+
+    seconds_since_unix_epoch = int(datetime.datetime.now().strftime("%s"))
+    seconds_since_1900_epoch = seconds_since_unix_epoch + seconds_delta
+
+    return seconds_since_1900_epoch
+
 def recv_all_bytes(rec_s):
   # taken from in class 
   data = b''
@@ -26,5 +42,9 @@ def recv_all_bytes(rec_s):
   return data
 
 byte_data_rec = recv_all_bytes(s)
+server_seconds_elapsed = int.from_bytes(byte_data_rec, 'big')
+system_seconds_elapsed = system_seconds_since_1900()
+s.close()
 
-print("Time: ", int.from_bytes(byte_data_rec, 'big'))
+print("NIST time  :", server_seconds_elapsed)
+print("System time:", system_seconds_elapsed)
